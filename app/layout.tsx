553cafe3c4e507f56541f58cf3096b73394ffec2
fransiskaartha <fransiskaarthaa@gmail.com>
2024-03-06 19:58:36 +0700

    DONE_UGD_2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-//jjjjj
 
 export default function RootLayout({
   children,
@@ -13,33 +12,3 @@ export default function RootLayout({
     </html>
   );
 }
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <style>
-//           {`
-//             .logo_hero.png {
-//               position: absolute;
-//               top: 20px; /* Sesuaikan posisi vertikal */
-//               left: 20px; /* Sesuaikan posisi horizontal */
-//               z-index: 1000; /* Pastikan z-index lebih tinggi dari background */
-//             }
-//           `}
-//         </style>
-//       </head>
-//       <body className={`${inter.className} antialiased`}>
-//         <img
-//           src="/logo_hero.png" // Ganti dengan path gambar logo Anda
-//           alt="logo_hero.png" // Ganti dengan deskripsi logo jika diperlukan
-//           className="logo_hero.png"
-//         />
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
